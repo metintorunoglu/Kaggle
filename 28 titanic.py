2f8274e44fe07d2d6c8262042c89dfc4046cc61a
metintorunoglu <45499397+metintorunoglu@users.noreply.github.com>
2019-05-19 17:09:50 +0300

    Add files via upload

---
+++ 28 titanic.py
@@ -0,0 +1,153 @@
+#
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+# Importing the dataset
+gender_submission = pd.read_csv('gender_submission.csv')
+test=pd.read_csv('test.csv')
+data=pd.read_csv('train.csv')
+
+ #checking for total null values
+data.isnull().sum()
+
+f,ax=plt.subplots(1,2, figsize=(10,5))
+data['Survived'].value_counts().plot.pie(explode=[0,0.1],autopct='%1.1f%%',ax=ax[0])
+ax[0].set_title('Survived')
+ax[0].set_ylabel('')
+sns.countplot('Survived',data=data,ax=ax[1])
+ax[1].set_title('Survived')
+plt.show()
+
+data.groupby(['Sex','Survived'])['Survived'].count()
+f,ax=plt.subplots(1,2,figsize=(10,5))
+data[['Sex','Survived']].groupby(['Sex']).mean().plot.bar(ax=ax[0])
+ax[0].set_title('Survived vs Sex')
+sns.countplot('Sex',hue='Survived',data=data,ax=ax[1])
+ax[1].set_title('Sex:Survived vs Dead')
+plt.show()
+
+f,ax=plt.subplots(1,2,figsize=(10,5))
+data['Pclass'].value_counts().plot.bar(color=['#CD7F32','#FFDF00','#D3D3D3'],ax=ax[0])
+ax[0].set_title('Number Of Passengers By Pclass')
+ax[0].set_ylabel('Count')
+sns.countplot('Pclass',hue='Survived',data=data,ax=ax[1])
+ax[1].set_title('Pclass:Survived vs Dead')
+plt.show()
+
+sns.factorplot('Pclass','Survived',hue='Sex',data=data)
+plt.show()
+
+print('Oldest Passenger was of:',data['Age'].max(),'Years')
+print('Youngest Passenger was of:',data['Age'].min(),'Years')
+print('Average Age on the ship:',data['Age'].mean(),'Years')
+
+f,ax=plt.subplots(1,2,figsize=(14,7))
+sns.violinplot("Pclass","Age", hue="Survived", data=data,split=True,ax=ax[0])
+ax[0].set_title('Pclass and Age vs Survived')
+ax[0].set_yticks(range(0,110,10))
+sns.violinplot("Sex","Age", hue="Survived", data=data,split=True,ax=ax[1])
+ax[1].set_title('Sex and Age vs Survived')
+ax[1].set_yticks(range(0,110,10))
+plt.show()
+
+
+data['Initial']=0
+for i in data:
+    data['Initial']=data.Name.str.extract('([A-Za-z]+)\.') #lets extract the Salutations
+data['Initial'].value_counts()
+data['Initial'].replace(['Mlle','Mme','Ms','Dr','Major','Lady','Countess','Jonkheer','Col','Rev','Capt','Sir','Don'],['Miss','Miss','Miss','Mr','Mr','Mrs','Mrs','Other','Other','Other','Mr','Mr','Mr'],inplace=True)
+data['Initial'].value_counts()
+data.groupby('Initial')['Age'].mean() #lets check the average age by Initials
+
+## Assigning the NaN Values with the Ceil values of the mean ages
+data.loc[(data.Age.isnull())&(data.Initial=='Mr'),'Age']=33
+data.loc[(data.Age.isnull())&(data.Initial=='Mrs'),'Age']=36
+data.loc[(data.Age.isnull())&(data.Initial=='Master'),'Age']=5
+data.loc[(data.Age.isnull())&(data.Initial=='Miss'),'Age']=22
+data.loc[(data.Age.isnull())&(data.Initial=='Other'),'Age']=46
+
+data.Age.isnull().any() #So no null values left finally 
+
+f,ax=plt.subplots(1,2,figsize=(14,7))
+data[data['Survived']==0].Age.plot.hist(ax=ax[0],bins=20,edgecolor='black',color='red')
+ax[0].set_title('Survived= 0')
+x1=list(range(0,85,5))
+ax[0].set_xticks(x1)
+data[data['Survived']==1].Age.plot.hist(ax=ax[1],color='green',bins=20,edgecolor='black')
+ax[1].set_title('Survived= 1')
+x2=list(range(0,85,5))
+ax[1].set_xticks(x2)
+plt.show()
+
+sns.factorplot('Pclass','Survived',col='Initial',data=data)
+plt.show()
+
+f,ax=plt.subplots(2,2,figsize=(14,7))
+sns.countplot('Embarked',data=data,ax=ax[0,0])
+ax[0,0].set_title('No. Of Passengers Boarded')
+sns.countplot('Embarked',hue='Sex',data=data,ax=ax[0,1])
+ax[0,1].set_title('Male-Female Split for Embarked')
+sns.countplot('Embarked',hue='Survived',data=data,ax=ax[1,0])
+ax[1,0].set_title('Embarked vs Survived')
+sns.countplot('Embarked',hue='Pclass',data=data,ax=ax[1,1])
+ax[1,1].set_title('Embarked vs Pclass')
+plt.subplots_adjust(wspace=0.2,hspace=0.5)
+plt.show()
+
+data['Embarked'].fillna('S',inplace=True)
+
+
+#Correlation Between The Features
+sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.2) #data.corr()-->correlation matrix
+fig=plt.gcf()
+fig.set_size_inches(10,8)
+plt.show()
+
+
+#Converting String Values into Numeric
+data['Sex'].replace(['male','female'],[0,1],inplace=True)
+data['Embarked'].replace(['S','C','Q'],[0,1,2],inplace=True)
+data['Initial'].replace(['Mr','Mrs','Miss','Master','Other'],[0,1,2,3,4],inplace=True)
+
+#Age_band
+data['Age_band']=0
+data.loc[data['Age']<=16,'Age_band']=0
+data.loc[(data['Age']>16)&(data['Age']<=32),'Age_band']=1
+data.loc[(data['Age']>32)&(data['Age']<=48),'Age_band']=2
+data.loc[(data['Age']>48)&(data['Age']<=64),'Age_band']=3
+data.loc[data['Age']>64,'Age_band']=4
+
+data['Age_band'].value_counts()
+
+#Dropping UnNeeded Features
+
+data.drop(['Name','Age','Ticket','Fare','Cabin', 'PassengerId'],axis=1,inplace=True)
+sns.heatmap(data.corr(),annot=True,cmap='RdYlGn',linewidths=0.2,annot_kws={'size':20})
+fig=plt.gcf()
+fig.set_size_inches(14,8)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.show()
+
+from sklearn.linear_model import LogisticRegression #logistic regression
+from sklearn import svm #support vector Machine
+from sklearn.ensemble import RandomForestClassifier #Random Forest
+from sklearn.neighbors import KNeighborsClassifier #KNN
+from sklearn.naive_bayes import GaussianNB #Naive bayes
+from sklearn.tree import DecisionTreeClassifier #Decision Tree
+from sklearn.model_selection import train_test_split #training and testing data split
+from sklearn import metrics #accuracy measure
+from sklearn.metrics import confusion_matrix #for confusion matrix
+
+
+
+
+
+
+
+
+
